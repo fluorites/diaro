90b57645136c284b744aa40a7f5489d9c7fd9a87
fluorites <fluorites@yandex.ru>
2017-01-13 17:44:18 +0500

    Добавлен шаблонный проект.

---
+++ Converter.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+//  Создание пространства имен Онуфрия Токарева.
+namespace Onuphrius {
+    // Создание пространства имен дневника.
+    namespace Diaro {
+        public class CConverter {
+        }
+
+        public class CTextConverter: CConverter {
+        }
+    }
+}
---
+++ ConverterForm.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+//  Создание пространства имен Онуфрия Токарева.
+namespace Onuphrius {
+    // Создание пространства имен дневника.
+    namespace Diaro {
+        public partial class CConverterForm: Form {
+            public CConverterForm() {
+                InitializeComponent();
+            }
+        }
+    }
+}
